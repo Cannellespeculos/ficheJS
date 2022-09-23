2e0e12fcf57dd981b2b6e633b148ae5177e1ad85
Ilyansm <cjeanpierre@edenschool.fr>
2022-09-23 14:59:49 +0200

    Type de variables et opérations sur les variables

--- js/app.js
+++ js/app.js
@@ -5,3 +5,24 @@ age = 16;
 console.log(typeof age);
 const prenom = "Cannelle";
 // le type de prenom est string
+let addition = 4+12;
+console.log(addition);
+let soustraction = 2-50;
+console.log(soustraction);
+let multiplication = 3*8;
+console.log(multiplication);
+let division = 7/2;
+console.log(division);
+let modulo = 5%2;
+console.log(modulo);
+let test = 3>5;
+console.log(test);
+console.log(typeof test);
+// le type de test est booléan
+// les opérateurs de comparaison sont <> === <= >= != == !==
+// les doubles égal comparent seulement la valeur alors que le triple comparent la valeur et le type 
+const identify ="Bonjour" + " " + prenom + " " + "vous avez" + " " + age + " " + "ans";
+console.log(identify);
+let majuscule = identify.toUpperCase();
+console.log(majuscule);
+// toUpperCase a rendu la variable de type string en majuscule
