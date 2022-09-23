039a8a9f9517f19528c978ac3384b65f7adfbd19
Ilyansm <cjeanpierre@edenschool.fr>
2022-09-23 16:10:50 +0200

    conditions

--- js/app.js
+++ js/app.js
@@ -26,3 +26,21 @@ console.log(identify);
 let majuscule = identify.toUpperCase();
 console.log(majuscule);
 // toUpperCase a rendu la variable de type string en majuscule
+age = prompt("Tapez votre age")
+if(age <= 0){
+    console.log("Vous etes un poisson (paner)")
+}
+if(age>0 && age<120){
+    console.log("Votre age est valider")
+}else{
+    console.log("je ne suis pas d'accord")
+}
+if(age>=21){
+    console.log("Vous etes majeur")
+}else if(age>18 && age<21) {
+    console.log("Vous etes majeur mais pas au USA ")
+    
+} else {
+    console.log("vous etes mineur")
+    
+}
